3e50365889145a37cf70f8c9378d091031ea5320
p.matrenin <p.matrenin@zebrains.team>
2021-03-10 10:04:05 +0400

    stoppableeventinterface

--- example.php
+++ example.php
@@ -5,6 +5,7 @@ require_once __DIR__ . '/vendor/autoload.php';
 use League\Container\Container;
 use Psr\EventDispatcher\EventDispatcherInterface;
 use Psr\EventDispatcher\ListenerProviderInterface;
+use Psr\EventDispatcher\StoppableEventInterface;
 use Simtel\PSR14Example\App;
 use Simtel\PSR14Example\EventDispatcher;
 use Simtel\PSR14Example\Events\EventOne;
@@ -14,11 +15,11 @@ use Simtel\PSR14Example\ListenerProvider;
 $container = new Container();
 $container->add(App::class)
     ->addArgument(EventOne::class)
-    ->addArgument(EventStoppable::class)
+    ->addArgument(StoppableEventInterface::class)
     ->addArgument(EventDispatcherInterface::class);
 
 $container->add(EventOne::class);
-$container->add(EventStoppable::class);
+$container->add(StoppableEventInterface::class, EventStoppable::class);
 $container->add(EventDispatcherInterface::class, EventDispatcher::class)->addArgument(ListenerProviderInterface::class);
 $container->add(ListenerProviderInterface::class, ListenerProvider::class);
 
